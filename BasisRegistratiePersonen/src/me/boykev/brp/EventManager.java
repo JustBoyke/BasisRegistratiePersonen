fdad7af4d87c539ccfffaea7f11924be9ed3e192
JustBoyke <b.vanvugt@fire-enterprise.nl>
2019-11-24 23:16:21 +0100

    Added an versioncheck

--- BasisRegistratiePersonen/src/me/boykev/brp/EventManager.java
+++ BasisRegistratiePersonen/src/me/boykev/brp/EventManager.java
@@ -8,6 +8,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import fr.onecraft.clientstats.ClientStats;
+import fr.onecraft.clientstats.ClientStatsAPI;
+
 public class EventManager implements Listener{
 	
 	public EventManager(Main main) {
@@ -18,10 +21,13 @@ public class EventManager implements Listener{
 	private UserManager um;
 	private SqlHandler sql;
 	
+	
 	@EventHandler
 	public void FirstJoin(PlayerJoinEvent e) {
+		ClientStatsAPI cstats = ClientStats.getApi();
 		sql = new SqlHandler(instance);
 		Player p = (Player) e.getPlayer();
+		int version = cstats.getProtocol(p.getUniqueId());
 		um = new UserManager(instance, p);
 		File configFile = new File(instance.getDataFolder() + File.separator + "users", p.getUniqueId().toString() + ".yml");
 		if(configFile.exists()) {
@@ -32,10 +38,18 @@ public class EventManager implements Listener{
 			um.editConfig().set("PlayerName", p.getName().toString());
 			um.save();
 			sql.insertUser(p);
+			if(version < 477) {
+				System.out.println("MC Version: " + version + " invopen skipped!");
+				return;
+			}
 			p.openInventory(InventoryManager.PrepareInv(p, "string"));
 			return;
 		}
 		if(um.getConfig().getString("Leeftijd") == null) {
+			if(version < 477) {
+				System.out.println("MC Version: " + version + " invopen skipped!");
+				return;
+			}
 			p.openInventory(InventoryManager.PrepareInv(p, "string"));
 			return;
 		}
